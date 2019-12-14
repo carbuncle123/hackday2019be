79b47351f0129a24e9b9e3a838ae0b9efbf5c759
carbuncle123 <ca.a1mandine.t@gmail.com>
2019-12-14 12:51:39 +0900

    :up: first spring

---
+++ src/main/java/com/ttsukiji/hackday2019be/Hackday2019beApplication.java
@@ -0,0 +1,13 @@
+package com.ttsukiji.hackday2019be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hackday2019beApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hackday2019beApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ttsukiji/hackday2019be/Hackday2019beApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ttsukiji.hackday2019be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hackday2019beApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
