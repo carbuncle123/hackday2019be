dc5ce510fd65f4f6a1a9ce07f8351d82300055c4
carbuncle123 <ca.a1mandine.t@gmail.com>
2019-12-14 18:36:49 +0900

    :up: dictパスに検索機能追加

--- src/main/java/com/ttsukiji/hackday2019be/controller/DictionaryEditController.java
+++ src/main/java/com/ttsukiji/hackday2019be/controller/DictionaryEditController.java
@@ -1,6 +1,7 @@
 package com.ttsukiji.hackday2019be.controller;
 
 import com.ttsukiji.hackday2019be.repository.DictionaryEditRepository;
+import com.ttsukiji.hackday2019be.repository.MySqlCategorizeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.HashMap;
 import java.util.List;
@@ -22,15 +24,25 @@ public class DictionaryEditController {
     @Qualifier("mySqlEditRepository")
     private DictionaryEditRepository mysqlRepository;
 
+    @Autowired
+    @Qualifier("mySqlCategorizeRepository")
+    private MySqlCategorizeRepository categorizeRepository;
+
     @GetMapping
     public String get(final Model model) {
-        final Map<Integer, List<String>> catWordMap = new HashMap<>();
-        for (int i = 1; i < 11; i++) {
-            catWordMap.put(i, mysqlRepository.getWords(i));
-        }
+        final Map<Integer, List<String>> catWordMap = crateCatWordMap();
         model.addAttribute("catWordMap", catWordMap);
         return "index";
     }
+
+    @PostMapping(path = "search")
+    public String search(@RequestParam(name = "search_word") final String word,
+                             final RedirectAttributes redirectAttributes){
+        final int categoryId = categorizeRepository.categorize(word).orElse(-1);
+        redirectAttributes.addFlashAttribute("categoryId", categoryId);
+        return "redirect:/dict";
+    }
+
 
     @PostMapping(path = "update")
     public String update(@RequestParam(name = "word") final String word,
@@ -38,4 +50,13 @@ public class DictionaryEditController {
         mysqlRepository.update(word, category_id);
         return "redirect:/dict";
     }
+
+    private Map<Integer, List<String>> crateCatWordMap() {
+        final Map<Integer, List<String>> catWordMap = new HashMap<>();
+        for (int i = 1; i < 11; i++) {
+            catWordMap.put(i, mysqlRepository.getWords(i));
+        }
+        return catWordMap;
+    }
+
 }
