945aba9c338ea67dcfcb1b1345271b7ff79a6cbe
carbuncle123 <ca.a1mandine.t@gmail.com>
2019-12-14 14:40:29 +0900

    :up: 多数決ロジック追加

--- src/main/java/com/ttsukiji/hackday2019be/service/CategorizeWithWordService.java
+++ src/main/java/com/ttsukiji/hackday2019be/service/CategorizeWithWordService.java
@@ -4,15 +4,15 @@ import com.atilika.kuromoji.ipadic.Token;
 import com.atilika.kuromoji.ipadic.Tokenizer;
 import com.ttsukiji.hackday2019be.domain.CategorizeResult;
 import com.ttsukiji.hackday2019be.repository.WordCategorizeRepository;
+import org.jooq.lambda.Seq;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.relational.core.sql.In;
+import org.springframework.http.converter.json.GsonBuilderUtils;
 import org.springframework.stereotype.Service;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 import static java.util.stream.Collectors.toList;
 
@@ -23,6 +23,8 @@ public class CategorizeWithWordService implements QueryCategorizeService {
     private List<String> targets = Arrays.asList("形容詞", "形容動詞", "名詞");
 
     private WordCategorizeRepository repository;
+
+    public static final int NOT_FOUND_ID = -1;
 
     @Autowired
     public CategorizeWithWordService(@Qualifier("mySqlCategorizeRepository") final WordCategorizeRepository repository,
@@ -44,15 +46,33 @@ public class CategorizeWithWordService implements QueryCategorizeService {
         final HashMap<String, Integer> detail = new HashMap<>();
         for (Token token: tokens) {
             final Optional<Integer> categoryId = this.repository.categorize(token.getBaseForm());
-            detail.put(token.getSurface(), categoryId.orElse(-1));
+            detail.put(token.getSurface(), categoryId.orElse(NOT_FOUND_ID));
         }
 
-        // todo: 多数決を取る
-        final int commonId = 1;
-        return new CategorizeResult(commonId, detail);
+        // 多数決
+        final List<Integer> ids = detail.values().stream().filter(id -> id != NOT_FOUND_ID).collect(toList());
+        return new CategorizeResult(mode(ids), detail);
     }
 
     private boolean isTarget(final Token token) {
         return targets.contains(token.getPartOfSpeechLevel1());
+    }
+
+    public static int mode(final List<Integer> ids) {
+        final Map<Integer, Integer> counts = new HashMap<>(); // {id: count}
+        for (int id: ids) {
+            final int count = counts.getOrDefault(id, 0) + 1;
+            counts.put(id, count);
+        }
+
+        int maxCount = -1;
+        int maxId = NOT_FOUND_ID;
+        for (Map.Entry<Integer, Integer> entry: counts.entrySet()) {
+            if (entry.getValue() > maxCount) {
+                maxCount = entry.getValue();
+                maxId = entry.getKey();
+            }
+        }
+        return maxId;
     }
 }
